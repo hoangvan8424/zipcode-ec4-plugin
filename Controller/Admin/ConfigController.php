21457293a0ea5dd11367fb03b4c7e413b75ae0ac
ThaiHV <hoangvan181198@gmail.com>
2023-06-06 09:35:23 +0700

    add code

---
+++ Controller/Admin/ConfigController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Plugin\Zipcode2\Controller\Admin;
+
+use Eccube\Controller\AbstractController;
+use Plugin\Zipcode2\Form\Type\Admin\ConfigType;
+use Plugin\Zipcode2\Repository\ConfigRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfigController extends AbstractController
+{
+    /**
+     * @var ConfigRepository
+     */
+    protected $configRepository;
+
+    /**
+     * ConfigController constructor.
+     *
+     * @param ConfigRepository $configRepository
+     */
+    public function __construct(ConfigRepository $configRepository)
+    {
+        $this->configRepository = $configRepository;
+    }
+
+    /**
+     * @Route("/%eccube_admin_route%/zipcode2/config", name="zipcode2_admin_config")
+     * @Template("@Zipcode2/admin/config.twig")
+     */
+    public function index(Request $request)
+    {
+        $Config = $this->configRepository->get();
+        $form = $this->createForm(ConfigType::class, $Config);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Config = $form->getData();
+            $this->entityManager->persist($Config);
+            $this->entityManager->flush();
+            $this->addSuccess('登録しました。', 'admin');
+
+            return $this->redirectToRoute('zipcode2_admin_config');
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
+}
---
+++ Nav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\Zipcode2;
+
+use Eccube\Common\EccubeNav;
+
+class Nav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [];
+    }
+}
